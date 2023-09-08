66bb33b856d844f274e96c107862ea7a5315e475
Adoghor Elvis <adoghorelvis@gmail.com>
2023-09-08 05:37:49 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 /**
- * main - Prints numbers between 0 to 9 and letters between a to f.
- *
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i;
-for (i = 48; i < 58; i++)
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
 {
-putchar(i);
-}
-for (i = 97; i < 103; i++)
-{
-putchar(i);
+putchar(ch);
 }
 putchar('\n');
 return (0);
