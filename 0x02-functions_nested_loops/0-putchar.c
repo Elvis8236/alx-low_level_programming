b37390db99a42ad7ebc28aad639c2602b23b9f8a
Adoghor Elvis <adoghorelvis@gmail.com>
2023-09-12 14:21:54 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * main - print _putchar
- *   * is written by mbah
- *    * Return: 0
- *     */
+ * * main - print _putchar
+ * * is written by mbah
+ * * Return: 0
+ * */
 int main(void)
 {
 	_putchar('_');
@@ -15,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
+	 return (0);
 }
