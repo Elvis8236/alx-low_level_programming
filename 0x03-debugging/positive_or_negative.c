0de477e4b851d30eba937b22cb87fe1a37cfbadb
Adoghor Elvis <adoghorelvis@gmail.com>
2023-09-14 07:59:42 -0700

    new

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,11 +6,11 @@
  */
 void positive_or_negative(int i)
 {
-if (i < 0)
+if (i > 0)
 {
 printf("%d is %s\n", i, "negative");
 }
-else if (i > 0)
+else if (i < 0)
 {
 printf("%d is %s\n", i, "positive");
 }
