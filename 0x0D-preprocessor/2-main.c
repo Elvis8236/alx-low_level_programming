56ea166bf3dd815d8604974b6684a2326a0bc807
Adoghor Elvis <adoghorelvis@gmail.com>
2023-10-10 10:21:29 -0700

    new task

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: Prints the name of the file
+ * main - prints the name of the file
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
